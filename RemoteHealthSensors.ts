7992c85a6aacad59d0067458cea60f65991ffa7d
kaho-vtc <64235624+kaho-vtc@users.noreply.github.com>
2025-05-15 14:07:56 +0800

    Update pxt.json, main.blocks, main.ts, RemoteHealthSensors.ts

--- RemoteHealthSensors.ts
+++ RemoteHealthSensors.ts
@@ -18,4 +18,35 @@ namespace HealthSensors {
 
         return result;
     }
+
+
+    // MAX30102
+    let max30102_addr = 0xAE;
+    let MAX30105_MODECONFIG = 0x09;
+    let MAX30105_MODE_MASK = 0xF8;
+    //% block
+    export function init_heart_rate_sensor()
+    {
+        gatorParticle.begin()
+    }
+    
+    //% block
+    export function read_pulse(): number 
+    {
+        /*
+        pins.i2cWriteNumber(max30102_addr, 0x07, NumberFormat.UInt8LE, true);
+        let buff = pins.i2cReadBuffer(max30102_addr, 3, false);
+        let result = buff[0];
+        result |= buff[1] << 8;
+        result *= 0.02;
+        result -= 273.15;
+        */
+        let result = gatorParticle.heartbeat(HeartbeatType.BPM);
+        if (result < 40 || result > 180)
+        {
+            result = 0;
+        }
+        return result;
+    }
+    
 }
